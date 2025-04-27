7062f13b8046eddefad81af8fd59d7837c1aa690
GoTears <127770095+GoTears@users.noreply.github.com>
2025-04-27 20:08:01 +0800

    用户中心最新

--- src/main/java/com/tears/usercenter/service/UserService.java
+++ src/main/java/com/tears/usercenter/service/UserService.java
@@ -9,5 +9,12 @@ import com.baomidou.mybatisplus.extension.service.IService;
 * @createDate 2025-04-27 19:21:32
 */
 public interface UserService extends IService<User> {
-
+    /**
+     * 用户注册
+     * @param userAccount 用户账户
+     * @param userPassword 用户密码
+     * @param checkPassword 校验密码
+     * @return 新用户 id
+     */
+    long userResister(String userAccount, String userPassword, String checkPassword);
 }
--- src/main/java/com/tears/usercenter/service/impl/UserServiceImpl.java
+++ src/main/java/com/tears/usercenter/service/impl/UserServiceImpl.java
@@ -4,9 +4,13 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.tears.usercenter.model.domain.User;
 import com.tears.usercenter.service.UserService;
 import com.tears.usercenter.mapper.UserMapper;
+import io.netty.util.internal.StringUtil;
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 /**
+ * 用户实现类
 * @author Taco
 * @description 针对表【user(用户)】的数据库操作Service实现
 * @createDate 2025-04-27 19:21:31
@@ -15,6 +19,14 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl extends ServiceImpl<UserMapper, User>
     implements UserService{
 
+    @Override
+    public long userResister(String userAccount, String userPassword, String checkPassword) {
+        //1.check
+        if (StringUtils.isAnyEmpty(userAccount, userPassword, checkPassword)){
+            return 0;
+        }
+
+    }
 }
 
 
