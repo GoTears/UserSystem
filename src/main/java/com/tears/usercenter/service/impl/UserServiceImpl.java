f93523f1ef8d951a72b5cad917744eaa91a2d39a
GoTears <127770095+GoTears@users.noreply.github.com>
2025-04-28 12:57:08 +0800

    用户中心最新

--- src/main/java/com/tears/usercenter/service/impl/UserServiceImpl.java
+++ src/main/java/com/tears/usercenter/service/impl/UserServiceImpl.java
@@ -1,11 +1,10 @@
 package com.tears.usercenter.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.tears.usercenter.mapper.UserMapper;
 import com.tears.usercenter.model.domain.User;
 import com.tears.usercenter.service.UserService;
-import com.tears.usercenter.mapper.UserMapper;
-import io.netty.util.internal.StringUtil;
-import org.apache.commons.lang3.StringEscapeUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
@@ -23,8 +22,23 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
     public long userResister(String userAccount, String userPassword, String checkPassword) {
         //1.check
         if (StringUtils.isAnyEmpty(userAccount, userPassword, checkPassword)){
-            return 0;
+            return -1;
         }
+        if (userAccount.length()<4 ){
+            return -1;
+        }
+        if(userPassword.length() < 8 || checkPassword.length() < 8){
+            return -1;
+        }
+        //账户重复查询
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("userAccount", userAccount);
+        long count = this.count();
+        if (count > 0){
+            return -1;
+        }
+        // 账号不能包含特殊字符
+        return 0;
 
     }
 }
