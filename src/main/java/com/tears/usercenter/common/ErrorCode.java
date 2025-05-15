2fd6b2cc508bb4fc57cde3f8eca70844dc117439
Fanghao Fu <FanghaoFu@axisoft.com>
2025-05-15 18:10:06 +0800

    通用返回处理类编写（未完成）

---
+++ src/main/java/com/tears/usercenter/common/ErrorCode.java
@@ -0,0 +1,36 @@
+package com.tears.usercenter.common;
+
+public enum ErrorCode {
+
+
+
+    private final int code;
+
+    /**
+     * 状态码信息
+     */
+    private final String message;
+
+    /**
+     * 状态码描述（详情）
+     */
+    private final String description;
+
+    ErrorCode(int code, String message, String description) {
+        this.code = code;
+        this.message = message;
+        this.description = description;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
---
+++ src/main/java/com/tears/usercenter/common/ResultUtils.java
@@ -0,0 +1,25 @@
+package com.tears.usercenter.common;
+
+public class ResultUtils {
+
+    /**
+     * 成功
+     * @param data
+     * @return
+     * @param <T>
+     */
+    public static <T> BaseResponse<T> success(T data){
+        return new BaseResponse<>(0, data, "ok");
+    }
+
+    /**
+     * 失败
+     * @param errorCode
+     * @return
+     */
+    public static BaseResponse error(ErrorCode errorCode) {
+        return new BaseResponse<>(errorCode);
+    }
+
+
+}
